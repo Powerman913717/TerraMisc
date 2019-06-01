6ce012f5c7ee2aadda79bd9daf78a1d034a4088b
Powerman913717 <powerman913717@gmail.com>
2019-05-31 21:23:37 -0400

    Renamed MetaStrings to GlobalTFCM

--- src/main/java/terramisc/common/GlobalTFCM.java
+++ src/main/java/terramisc/common/GlobalTFCM.java
@@ -1,7 +1,11 @@
 package terramisc.common;
 
-public class MetaStrings 
+import com.bioxx.tfc.api.Metal;
+
+public class GlobalTFCM 
 {
 	public static final String[] ClayColors = new String[] {"black", "blue", "brown", "cyan", "gray", 
 		"green", "lightblue", "lightgray", "lime", "magenta", "orange", "pink", "purple", "red", "white"};
+	
+	public static Metal LEADPEWTER;
 }
